a81e824e2e511ce0327a8273035cc7f7c812dcdd
Soumyajit Roy <roysoumyajit36@gmail.com>
2024-07-15 09:47:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pages
+from menu import menu
+
+st.set_page_config(
+    page_title="Vehicle Registration and Monitoring App",
+    page_icon="🚗",
+    layout="wide",
+    initial_sidebar_state = "collapsed"
+)
+
+from pages import home
+home.main()
+
+
